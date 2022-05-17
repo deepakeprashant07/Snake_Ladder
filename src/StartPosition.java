672e4fb865b9964f165efec7b08f34227eeb9406
Prashant Deepake <deepakeprashant1@gmail.com>
2022-05-17 23:40:05 +0530

    ADDED : Player Position Start By Zero

---
+++ src/StartPosition.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class StartPosition {
+    public static void main(String[] args) {
+        int Player_Position=0;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Welcome To Snake And Ladder Game");
+        System.out.println("Are You Ready Press Y Or N");
+        String start=scan.next();
+        while ("y".equals(start)){
+            System.out.println("Player Position " +Player_Position);
+            break;
+        }
+    }
+}
